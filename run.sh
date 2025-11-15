ccafe02fc9b7431d56815fe42065494553465a76
xiyuanyang-code <xiyuan__yang@outlook.com>
2025-11-15 19:55:11 +0800

    update: update resume for release v1.3.5

--- run.sh
+++ run.sh
@@ -17,8 +17,24 @@ else
   exit 1
 fi
 
+# generate new tags
+LATEST_TAG=$(git tag -l 'v*' --sort=-v:refname | head -1)
+LATEST_TAG=${LATEST_TAG:-v0.0.0}
+VERSION_NO_V=$(echo "$LATEST_TAG" | sed 's/^v//')
+IFS='.' read -r -a VERSION_ARRAY <<< "$VERSION_NO_V"
+echo "Latest Tag: $LATEST_TAG"
+
+MAJOR=${VERSION_ARRAY[0]}
+MINOR=${VERSION_ARRAY[1]}
+PATCH=${VERSION_ARRAY[2]}
+NEXT_PATCH=$((PATCH + 1))
+NEW_TAG="v$MAJOR.$MINOR.$NEXT_PATCH"
+echo "New tag: $NEW_TAG"
 
 git add .
-git commit -m "update: update resume"
+git commit -m "update: update resume for release $NEW_TAG"
 git push
 
+# pushing new tags
+git tag "$NEW_TAG"
+git push origin "$NEW_TAG"
